ec4cd6fb14369704c29b48a37be4db1b9c124049
Luis Fernando Cezar dos Santos <nandocezar22@gmail.com>
2022-10-22 22:08:55 -0300

    Init project

---
+++ src/main/java/com/company/app/App.java
@@ -0,0 +1,13 @@
+package com.company.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
