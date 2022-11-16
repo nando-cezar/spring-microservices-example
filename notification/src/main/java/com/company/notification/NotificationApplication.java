8b43c6a40279f89de7b58a4f9afa644dd99428a8
nando-cezar <nandocezar22@gmail.com>
2022-11-16 16:18:30 -0300

    init implements rabbitMQ

--- notification/src/main/java/com/company/notification/NotificationApplication.java
+++ notification/src/main/java/com/company/notification/NotificationApplication.java
@@ -1,11 +1,36 @@
 package com.company.notification;
 
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+//import com.company.amqp.producer.RabbitMQMessageProducer;
+//import com.company.notification.config.NotificationConfig;
+
+@SpringBootApplication(
+    scanBasePackages = {
+        "com.company.notification",
+        "com.company.amqp"
+    }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    /*@Bean
+    CommandLineRunner commandLineRunner(
+        RabbitMQMessageProducer producer, 
+        NotificationConfig notificationConfig
+        ){
+        return args -> {
+            producer.publish(
+                new Person("Ali", 18), 
+                notificationConfig.getInternalExchange(), 
+                notificationConfig.getInternalNotificationRoutingKey());
+        };
+    }
+
+    record Person(String name, int age){}*/
 }
